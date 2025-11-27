852dd04ef3c6eaf25c44be0d64d4aca061a61de3
maelfrd <mael.frard@edu.ece.fr>
2025-11-27 10:01:48 +0100

    Update terrain.c

--- terrain.c
+++ terrain.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+   int y, x;
+
+
+for (y = 0; y <= 20; y++) {
+      for (x = 0; x <= 25; x++) {
+         if (y == 0 || y == 20) {
+          printf("-");
+
+        } else if (x == 0 || x == 25) {
+          printf("|");
+
+        } else {
+          printf(" ");
+        }
+      }
+      printf("\n");
+    }
+}
