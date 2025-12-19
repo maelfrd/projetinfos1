be267fa16f60e5240c197d76e077f3951ec29ec2
maelfrd <mael.frard@edu.ece.fr>
2025-12-19 22:12:34 +0100

    Update gestion_partie.h

--- gestion_partie.h
+++ gestion_partie.h
@@ -1,21 +1,23 @@
-/* gestion_partie.h */
-#ifndef GESTION_PARTIE_H
-#define GESTION_PARTIE_H
 
-#include "structures.h"
 
-void init_partie(Jeu *jeu, int lignes, int colonnes);
-void liberer_partie(Jeu *jeu);
-int verifier_victoire(Jeu *jeu);
-int gerer_echange(Jeu *jeu);
-int gerer_echange_n2(Jeu *jeu);
-void boucle_jeu(Jeu *jeu);
-void boucle_jeu_n2(Jeu *jeu);
-void boucle_jeu_n3(Jeu *jeu);
+/* gestion_partie.h */                                      /* En-tete du fichier */
+#ifndef GESTION_PARTIE_H                                    /* Protection contre inclusion multiple */
+#define GESTION_PARTIE_H                                    /* Debut de la definition du header */
 
-/* Sauvegarde */
-void sauvegarder_partie(Sauvegarde *sauv);
-int charger_sauvegarde(Sauvegarde *sauv);
-void supprimer_sauvegarde(void);
+#include "structures.h"                                     /* Inclut les structures du jeu */
 
-#endif
+void init_partie(Jeu *jeu, int lignes, int colonnes);       /* Initialise une nouvelle partie */
+void liberer_partie(Jeu *jeu);                              /* Libere la memoire du plateau */
+int verifier_victoire(Jeu *jeu);                            /* Verifie si objectifs atteints, retourne 1/0 */
+int gerer_echange(Jeu *jeu);                                /* Gere un echange niveau 1, retourne 0/-1 */
+int gerer_echange_n2(Jeu *jeu);                             /* Gere un echange niveau 2/3, retourne 0/-1 */
+void boucle_jeu(Jeu *jeu);                                  /* Boucle principale niveau 1 */
+void boucle_jeu_n2(Jeu *jeu);                               /* Boucle principale niveau 2 */
+void boucle_jeu_n3(Jeu *jeu);                               /* Boucle principale niveau 3 */
+
+/* Sauvegarde */                                            /* Section sauvegarde */
+void sauvegarder_partie(Sauvegarde *sauv);                  /* Ecrit sauvegarde dans fichier */
+int charger_sauvegarde(Sauvegarde *sauv);                   /* Lit sauvegarde depuis fichier */
+void supprimer_sauvegarde(void);                            /* Supprime le fichier de sauvegarde */
+
+#endif                                                      /* Fin de la protection d'inclusion */
