97ccfd689325ebab8a7daa48e8fef3c70a7fa40d
denariemarin-coder <denariemarin@gmail.com>
2025-12-19 17:15:28 +0100

    Update saisie.c

--- saisie.c
+++ saisie.c
@@ -1,7 +1,6 @@
-/* saisie.c - Gestion des entrees clavier */
 
-#include <stdio.h>      // Pour fgets() et stdin
-#include "saisie.h"     // Déclarations et structure Jeu
+#include <stdio.h>      
+#include "saisie.h"     
 
 // Lit une commande clavier et retourne un caractère
 char lire_commande(void)
@@ -9,7 +8,7 @@ char lire_commande(void)
     char buf[64];       // Buffer pour stocker la ligne saisie
     int i;              // Indice pour parcourir le buffer
     
-    // Lecture d'une ligne depuis l'entrée standard (clavier)
+    
     // Si erreur ou fin de fichier, on retourne 'x' (quitter)
     if (fgets(buf, sizeof(buf), stdin) == NULL) return 'x';
 
@@ -28,33 +27,33 @@ char lire_commande(void)
 // Applique la commande au jeu
 void appliquer_commande(Jeu *jeu, char cmd)
 {
-    // ----- Quitter le jeu -----
+    // Quitter le jeu
     if (cmd == 'x' || cmd == 'X') {
         jeu->en_cours = 0;   // Arrête la boucle principale du jeu
         return;             // Sort de la fonction
     }
     
-    // ----- Déplacement vers le haut (Z) -----
+    //Déplacement vers le haut (Z)
     if (cmd == 'z' || cmd == 'Z') {
         if (jeu->curseur_x > 0)          // Vérifie qu'on ne sort pas de la grille
             jeu->curseur_x--;             // Déplace le curseur vers le haut
     }
-    // ----- Déplacement vers le bas (S) -----
+    // Déplacement vers le bas (S) 
     else if (cmd == 's' || cmd == 'S') {
         if (jeu->curseur_x < jeu->lignes - 1)
             jeu->curseur_x++;             // Déplace le curseur vers le bas
     }
-    // ----- Déplacement vers la gauche (Q) -----
+    // Déplacement vers la gauche (Q)
     else if (cmd == 'q' || cmd == 'Q') {
         if (jeu->curseur_y > 0)
             jeu->curseur_y--;             // Déplace le curseur vers la gauche
     }
-    // ----- Déplacement vers la droite (D) -----
+    // Déplacement vers la droite (D)
     else if (cmd == 'd' || cmd == 'D') {
         if (jeu->curseur_y < jeu->colonnes - 1)
             jeu->curseur_y++;             // Déplace le curseur vers la droite
     }
-    // ----- Sélection (P) -----
+    // Sélection (P) 
     else if (cmd == 'p' || cmd == 'P') {
         // Si aucune sélection n'est active
         if (!jeu->selection) {
