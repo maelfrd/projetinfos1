83f13056245b0b7e08eb780a8fa59b2b1eca7ca0
maelfrd <mael.frard@edu.ece.fr>
2025-12-19 22:19:21 +0100

    Update saisie.h

--- saisie.h
+++ saisie.h
@@ -1,10 +1,12 @@
-/* saisie.h */
-#ifndef SAISIE_H
-#define SAISIE_H
 
-#include "structures.h"
+/* saisie.h */                                              /* En-tete du fichier */
+#ifndef SAISIE_H                                            /* Protection contre inclusion multiple */
+#define SAISIE_H                                            /* Debut de la definition du header */
 
-char lire_commande(void);
-void appliquer_commande(Jeu *jeu, char cmd);
+#include "structures.h"                                     /* Inclut les structures du jeu */
 
-#endif
+char lire_commande(void);                                   /* Lit une touche clavier, retourne caractere */
+void appliquer_commande(Jeu *jeu, char cmd);                /* Applique commande au jeu (deplacement/selection) */
+int appliquer_commande_menu(char cmd);                      /* Applique commande menu, retourne action */
+
+#endif                                                      /* Fin de la protection d'inclusion */
