8ee614cc07b6c1e0c5beb5f9607a9f517abb6599
maelfrd <mael.frard@edu.ece.fr>
2025-12-19 17:30:45 +0100

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -4,12 +4,12 @@
 
 #include "structures.h"
 
-void nettoyer_ecran(void);
+void nettoyer_ecran(void);// permet de nettoyer l'affichage de l'ecran cet a dire faire descendre le terminal 
 void pause_entree(void);
-void afficher_plateau(Jeu *jeu);
-void afficher_infos(Jeu *jeu);
-void afficher_controles(Jeu *jeu);
-void afficher_jeu(Jeu *jeu);
-void afficher_fin(Jeu *jeu);
+void afficher_plateau(Jeu *jeu);// affichage du plateau 
+void afficher_infos(Jeu *jeu);//afficher ce qui est definis dans les infos et objectifs de reussite du niveau 
+void afficher_controles(Jeu *jeu);//
+void afficher_jeu(Jeu *jeu);//
+void afficher_fin(Jeu *jeu);//afficher le  cas de reussite du jeu 
 
 #endif
