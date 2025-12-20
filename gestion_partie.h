d65f267415359081655421ca4f0537fe89960d23
maelfrd <mael.frard@edu.ece.fr>
2025-12-20 21:24:03 +0100

    Update gestion_partie.h

--- gestion_partie.h
+++ gestion_partie.h
@@ -8,7 +8,7 @@ void liberer_partie(Jeu *jeu);                              /* Libere la memoire
 int verifier_victoire(Jeu *jeu);                            /* il vérifie que les objectifs soit bien atteints, retourne 1/0 */
 int gerer_echange(Jeu *jeu);                                /* veriffie les echanges pour le niveau 1 */
 int gerer_echange_n2(Jeu *jeu);                             /* Verifie les echanges pour le niveau 2 et 3*/
-void boucle_jeu(Jeu *jeu);                                  /* Boucle principale niveau 1 */
+void boucle_jeu(Jeu *jeu);                                  /* Boucle (etapes de fonctionnement du niveau )principale niveau 1 */
 void boucle_jeu_n2(Jeu *jeu);                               /* Boucle principale niveau 2 */
 void boucle_jeu_n3(Jeu *jeu);                               /* Boucle principale niveau 3 */
 
