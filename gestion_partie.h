ce6a0511cc9f5a60be1eac7d19466c5159f4ce85
lancelot1805 <lancelot.colinet@edu.ece.fr>
2025-12-20 17:52:59 +0100

    Update gestion_partie.h

--- gestion_partie.h
+++ gestion_partie.h
@@ -6,15 +6,15 @@
 void init_partie(Jeu *jeu, int lignes, int colonnes);     
 void liberer_partie(Jeu *jeu);                              /* Libere la memoire du plateau */
 int verifier_victoire(Jeu *jeu);                            /* il vérifie que les objectifs soit bien atteints, retourne 1/0 */
-int gerer_echange(Jeu *jeu);                                /* Gere un echange niveau 1, retourne 0/-1 */
-int gerer_echange_n2(Jeu *jeu);                             /* Gere un echange niveau 2/3, retourne 0/-1 */
+int gerer_echange(Jeu *jeu);                                /* veriffie les echanges pour le niveau 1 */
+int gerer_echange_n2(Jeu *jeu);                             /* Verifie les echanges pour le niveau 2 et 3*/
 void boucle_jeu(Jeu *jeu);                                  /* Boucle principale niveau 1 */
 void boucle_jeu_n2(Jeu *jeu);                               /* Boucle principale niveau 2 */
 void boucle_jeu_n3(Jeu *jeu);                               /* Boucle principale niveau 3 */
 
 /* Sauvegarde */                                            
-void sauvegarder_partie(Sauvegarde *sauv);                  /* Ecrit sauvegarde dans fichier */
+void sauvegarder_partie(Sauvegarde *sauv);                  /* Ecrit la sauvegarde dans fichier */
 int charger_sauvegarde(Sauvegarde *sauv);                   /* Lit sauvegarde depuis fichier */
 void supprimer_sauvegarde(void);                            /* Supprime le fichier de sauvegarde */
 
-#endif                                                      /* Fin de la protection d'inclusion */
+#endif                                                   
