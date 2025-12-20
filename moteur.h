adc4be3ef984589b34d2c113ef19e1e0f56a3b91
antoinernlt <antoinerenoultbussac@icloud.com>
2025-12-20 18:33:54 +0100

    Refactor moteur.h for improved readability

--- moteur.h
+++ moteur.h
@@ -1,39 +1,39 @@
 
                        
-#ifndef MOTEUR_H                                            /* Protection contre inclusion multiple */
-#define MOTEUR_H                                            /* Debut de la definition du header */
+#ifndef MOTEUR_H                                            
+#define MOTEUR_H                                            
 
-#include "structures.h"                                     /* Inclut les structures du jeu */
+#include "structures.h"                                    
 
-/* Fonctions de base */                                  
-int est_special(char *c);                                   /* Verifie si l'emoji est special (bombe, etc.) */
-int est_joker(char *c);                                     /* Verifie si l'emoji est un joker */
-int est_fruit(char *c);                                     /* Verifie si l'emoji est un fruit normal */
-int index_fruit(Jeu *jeu, char *f);                         /* Retourne index du fruit (0-4) ou -1 */
+// Fonctions de base                                   
+int est_special(char *c);                                   
+int est_joker(char *c);                                     
+int est_fruit(char *c);                                    
+int index_fruit(Jeu *jeu, char *f);                         
 
-/* Menu */                                                  
-int menu(Sauvegarde *sauv);                                 /* Affichage du menu et retourne choix utilisateur */
+/Affichage du menu et retourne choix utilisateur                                                   
+int menu(Sauvegarde *sauv);                                 
 
-/* Alignements */                                           
-int chercher_alignements(Jeu *jeu, int marques[]);          /* Trouve les alignements de 3+, remplit marques[] */
-void supprimer_marques(Jeu *jeu, int marques[]);            /* Supprime les cases marquees, on compte les points */
-void faire_tomber(Jeu *jeu);                                /* Applique la gravite (les fruits tombent) */
-void remplir_vides(Jeu *jeu, int niveau2);                  /* Remplit les cases vides avec des nouveaux fruits */
+// Alignements                                           
+int chercher_alignements(Jeu *jeu, int marques[]);          
+void supprimer_marques(Jeu *jeu, int marques[]);            
+void faire_tomber(Jeu *jeu);                                
+void remplir_vides(Jeu *jeu, int niveau2);                  
 
-/* Permutations */                                         
-void echanger(Jeu *jeu, int x1, int y1, int x2, int y2);    /* Echange entre les deux cases adjacentes */
-int permutation_valide(Jeu *jeu, int x1, int y1, int x2, int y2);  /* Verifie si l'echange cree un alignement */
-void cascade(Jeu *jeu);                                     /* Boucle suppression/gravite/remplissage (N1) */
-void cascade_niveau2(Jeu *jeu);                             /* Cascade avec detection speciaux (N2/N3) */
+// Permutations                                         
+void echanger(Jeu *jeu, int x1, int y1, int x2, int y2);   
+int permutation_valide(Jeu *jeu, int x1, int y1, int x2, int y2); 
+void cascade(Jeu *jeu);                                     
+void cascade_niveau2(Jeu *jeu);                             
 
-/* Combinaisons speciales niveau 2 */                      
-int detecter_speciaux(Jeu *jeu, int marques[], int speciaux[]);  /* Detecte les formations speciales */
-int permutation_valide_n2(Jeu *jeu, int x1, int y1, int x2, int y2);  /* Validation echange niveau 2 */
+// Combinaisons speciales niveau 2                      
+int detecter_speciaux(Jeu *jeu, int marques[], int speciaux[]);  
+int permutation_valide_n2(Jeu *jeu, int x1, int y1, int x2, int y2);  
 
-/* Effets des bonbons speciaux */                           
-void effet_colonne(Jeu *jeu, int y);                        /* Arbre: supprime une colonne entiere */
-void effet_ligne(Jeu *jeu, int x);                          /* Boomerang: supprime une ligne entiere */
-void effet_bombe(Jeu *jeu, int x, int y);                   /* Bombe: supprime une zone 3x3 */
-void effet_arcenciel(Jeu *jeu, char *cible);                /* Arc-en-ciel: supprime tous fruits d'un type dans tout le plateau */
+// Effets des bonbons speciaux                            
+void effet_colonne(Jeu *jeu, int y);                        
+void effet_ligne(Jeu *jeu, int x);                          
+void effet_bombe(Jeu *jeu, int x, int y);                   
+void effet_arcenciel(Jeu *jeu, char *cible);                
 
-#endif                                                      /* Fin de la protection d'inclusion */
+#endif                                                      
